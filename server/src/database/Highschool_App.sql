808594a64851e3a33c58964262022f4c6f1be190
Alejandro Gimeno <32788331+AlexGA93@users.noreply.github.com>
2022-06-25 14:15:11 +0000

    Update Highschool_App.sql
    Update Highschool_App.sql

--- server/src/database/Highschool_App.sql
+++ server/src/database/Highschool_App.sql
@@ -1,217 +1,89 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jun 05, 2022 at 10:00 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- create database
+CREATE DATABASE HighSchool_App;
+-- access to the database
+USE HighSchool_App;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- TABLES
+-- Highschools
+CREATE TABLE HighSchools(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    contact_phone VARCHAR(255) NOT NULL,
+    web VARCHAR(255) NOT NULL
+);
+-- Teachers
+CREATE TABLE Teachers(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    second_name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    contact_phone VARCHAR(255) NOT NULL,
+    educational_center INT NOT NULL
+);
+-- Highschools/Teachers Relationship - a E.C can have a lot of teachers, but any teacher has one single E.C
+ALTER TABLE Teachers
+    ADD CONSTRAINT FK_TEACHERS_HIGHSCHOOL
+    FOREIGN KEY(educational_center)
+    REFERENCES HighSchools(id);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Students
+CREATE TABLE Students(
+    id INT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    second_name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(255), 
+    PRIMARY KEY(id)
+);
 
---
--- Database: `Highschool_App`
---
+-- Subjects
+CREATE TABLE Subjects(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    subject_name VARCHAR(255) NOT NULL,
+    exam_date DATE NOT NULL, -- format: YYYY-MM-DD
+    qualification FLOAT NOT NULL,
+    student_id INT NOT NULL
+);
 
--- --------------------------------------------------------
+-- Evaluation
+CREATE TABLE Evaluation(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    subject_name VARCHAR(255) NOT NULL,
+    final_qualification FLOAT NOT NULL,
+    student_id INT NOT NULL
+);
 
---
--- Table structure for table `Classroom`
---
+-- Students/Subjects Relationship
+ALTER TABLE Subjects
+    ADD CONSTRAINT FK_SUBJECTS_STUDENTS
+    FOREIGN KEY(student_id)
+    REFERENCES Students(id);
 
-CREATE TABLE `Classroom` (
-  `id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Students/Evaluation Relationship
+ALTER TABLE Evaluation
+    ADD CONSTRAINT FK_EVALUATION_STUDENTS
+    FOREIGN KEY(student_id)
+    REFERENCES Students(id);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `Evaluation`
---
 
-CREATE TABLE `Evaluation` (
-  `id` int(11) NOT NULL,
-  `subject` varchar(45) NOT NULL,
-  `final_qual` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Classroom
+CREATE TABLE Classroom(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    teacher_id INT NOT NULL,
+    student_id INT NOT NULL
+);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `HighSchools`
---
-
-CREATE TABLE `HighSchools` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `contact_phone` varchar(100) NOT NULL,
-  `web` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Students`
---
-
-CREATE TABLE `Students` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `age` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `subject_id` int(11) NOT NULL,
-  `final_eval_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Subjects`
---
-
-CREATE TABLE `Subjects` (
-  `id` int(11) NOT NULL,
-  `subjects` varchar(45) NOT NULL,
-  `qualification` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Teachers`
---
-
-CREATE TABLE `Teachers` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `age` int(11) NOT NULL,
-  `contact_phone` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `highschool_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Classroom`
---
-ALTER TABLE `Classroom`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `teacher_fk` (`teacher_id`),
-  ADD KEY `student_fk` (`student_id`);
-
---
--- Indexes for table `Evaluation`
---
-ALTER TABLE `Evaluation`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `HighSchools`
---
-ALTER TABLE `HighSchools`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `Students`
---
-ALTER TABLE `Students`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `subject_fk` (`subject_id`),
-  ADD KEY `final_eval_fk` (`final_eval_id`);
-
---
--- Indexes for table `Subjects`
---
-ALTER TABLE `Subjects`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `Teachers`
---
-ALTER TABLE `Teachers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `highschool_fk` (`highschool_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Classroom`
---
-ALTER TABLE `Classroom`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Evaluation`
---
-ALTER TABLE `Evaluation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `HighSchools`
---
-ALTER TABLE `HighSchools`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Students`
---
-ALTER TABLE `Students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Subjects`
---
-ALTER TABLE `Subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Teachers`
---
-ALTER TABLE `Teachers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Classroom`
---
-ALTER TABLE `Classroom`
-  ADD CONSTRAINT `student_fk` FOREIGN KEY (`student_id`) REFERENCES `Students` (`id`),
-  ADD CONSTRAINT `teacher_fk` FOREIGN KEY (`teacher_id`) REFERENCES `Teachers` (`id`);
-
---
--- Constraints for table `Students`
---
-ALTER TABLE `Students`
-  ADD CONSTRAINT `final_eval_fk` FOREIGN KEY (`final_eval_id`) REFERENCES `Evaluation` (`id`),
-  ADD CONSTRAINT `subject_fk` FOREIGN KEY (`subject_id`) REFERENCES `Subjects` (`id`);
-
---
--- Constraints for table `Teachers`
---
-ALTER TABLE `Teachers`
-  ADD CONSTRAINT `highschool_fk` FOREIGN KEY (`highschool_id`) REFERENCES `HighSchools` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Teachers/Classroom Relationship
+ALTER TABLE Classroom
+    ADD CONSTRAINT FK_CLASSROOM_TEACHERS
+    FOREIGN KEY(teacher_id)
+    REFERENCES Teachers(id);
+-- Students/Classroom Relationship
+ALTER TABLE Classroom
+    ADD CONSTRAINT FK_CLASSROOM_STUDENTS
+    FOREIGN KEY(student_id)
+    REFERENCES Students(id);
