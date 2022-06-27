38d1fa8a5604b2567e201ba0e59f7015b805fe41
AlexGA93 <alexgimat@hotmail.com>
2022-06-27 17:32:24 +0000

    first sql controllers. Need to select database

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -1,18 +1,35 @@
+// import { MySQLdatabase } from "../types/type";
+import {Request, Response} from 'express';
 const mysql = require('mysql2');
 
 require('dotenv').config();
-console.log(
-    process.env.MYSQL_USER+"\n"+
-    process.env.MYSQL_PASSWORD+"\n"+
-    process.env.MYSQL_DATABASE+"\n"+
-    process.env.MYSQL_HOST);
 
-// const database = '';
-const database = mysql.createConnection({
-    user: process.env.MYSQL_USER,
-    host: process.env.MYSQL_HOST,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+const database = mysql.createConnection(
+    {
+        user: process.env.MYSQL_USER,// johndoe
+        host: process.env.MYSQL_HOST,// mysqldb
+        password: process.env.MYSQL_PASSWORD,// secret
+        // database: process.env.MYSQL_DATABASE // Highschool_App
+    }
+);
+
+database.connect((err: Error) => {
+    if (err) throw err;
+
+    console.log('Connected to MySQL!');
+
+    // connect to the database
+    database.query(
+        `CREATE DATABASE ${process.env.MYSQL_DATABASE};`,
+        (err:Error) => {
+            if(err) throw err;
+
+            console.log(`Database ${process.env.MYSQL_DATABASE} created!`);
+            
+        }
+    );
+    
 });
+
 
 module.exports = database;
--- server/src/router/router.ts
+++ server/src/router/router.ts
@@ -1,13 +1,30 @@
 
 import express from 'express';
-const controller = require('../controllers/controllers');
-import { body } from 'express-validator';
 const router = express.Router();
+const controller = require('../controllers/controllers');
+// import { body } from 'express-validator';
+
+router.get('/create-database', controller.createDatabase);
+
+router.get('/create', controller.createHighschool);
+router.get('/insert',controller.insertHighschool);
+router.get('/display', controller.displayHighschool);
 
 
-router.get('/', controller.initialProcess);
-// router.get('/', controller.initDatabase);
-router.get('/getHighSchools', controller.getHighSchoolsTable);
+// create table by table
+// router.get('/database/create/highschools');
+// router.get('/database/create/teachers');
+// router.get('/database/create/students');
+// router.get('/database/create/subjects');
+// router.get('/database/create/evaluations');
+// router.get('/database/create/classrooms');
+
+
+
+// router.get('/', controller.initialProcess);
+// router.get('/getHighSchools', controller.getHighSchoolsTable);
+// router.get('/database/full', controller.fullDatabase);
+// router.get('/database/displayH', controller.displayHighSchools);
 
 
 module.exports = router;
